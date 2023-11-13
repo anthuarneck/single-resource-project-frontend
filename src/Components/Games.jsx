15476181c9c4141bb6d21b9f013fca3d11b620a4
Joseph-Carter <1joseph1carter@gmail.com>
2023-11-12 19:14:51 -0500

    fixed games and game files, rendering proper data, working on games details to delete game based on userId

--- src/Components/Games.jsx
+++ src/Components/Games.jsx
@@ -1,41 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import Game from "./Game"
+import React, { useEffect, useState } from "react";
+import Game from "./Game";
 
 const API = import.meta.env.VITE_API_URL;
 
 const Games = () => {
-    const [games, setGames] = useState([]);
+  const [games, setGames] = useState([]);
 
-    useEffect(() => {
-        fetch(`${API}/games`)
-        .then(response => response.json())
-        .then((response) => {
-            console.log(response)
-            setGames(response.data.payload)
-        })
-        .catch((err) => {
-            console.error(err)
-        })
-    }, [])
+  useEffect(() => {
+    fetch(`${API}/games`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setGames(response.data.payload);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
-    return (
-        <div className='allGames'>
-            <section>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>See this game.</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {games.map((game) => {
-                            return <Game key={game.id} game={game}/>
-                        })}
-                    </tbody>
-                </table>
-            </section>
-        </div>
-    );
-}
+  return (
+    <div className="allGames">
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th>Available</th>
+            </tr>
+          </thead>
+          <tbody>
+            {games.map((game) => {
+              return <Game key={game.id} game={game} />;
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+};
 
 export default Games;
