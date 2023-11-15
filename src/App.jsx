4a1f3c213ba64080217dc9351633408c308caf49
Joseph-Carter <1joseph1carter@gmail.com>
2023-11-14 19:44:25 -0500

    Trying to get the details of the game to render,

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Login from "./Pages/Login";
 import GamesCart from "./Components/gamesCart";
 import FavoriteGames from "./Components/FavoriteGames";
 import Error from "./Pages/Error";
+import Show from "./Pages/Show";
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<SignUp />} />
           <Route path="/games" element={<Games />} />
+          <Route path="/games/:index" element={<Show/>} />
           <Route path="/cart" element={<GamesCart />} />
           <Route path="/users/:userId/favoritedGames" element={<FavoriteGames />} />
           <Route path="*" element={<Error />} />
--- src/Components/GameDetails.jsx
+++ src/Components/GameDetails.jsx
@@ -4,34 +4,52 @@ import { Link, useParams, useNavigate } from "react-router-dom";
 const API = import.meta.env.VITE_API_URL;
 
 const GameDetails = () => {
-  const [games, setGames] = useState([]);
-  const { id } = useParams();
-  const navigate = useNavigate();
+  const [game, setGame] = useState([]);
+  let { id } = useParams();
+  let navigate = useNavigate();
 
   useEffect(() => {
     fetch(`${API}/games/${id}`)
       .then((response) => response.json())
-      .then((response) => {
-        setGames(response.data.payload);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [id, API]);
+      .then((game) => setGame(game))
+      console.log(game)
+      .catch((navigate) => navigate(`/404`));
+  }, [id, navigate]);
+
 
   const handleDelete = () => {
     deleteGame()
   }
 
   const deleteGame = () => {
-    const httpOptions = { method: "DELETE" }
+    const httpOptions = { method: "DELETE" };
     fetch(`${API}/games/${id}`, httpOptions)
-        .then(() => navigate(`/games`))
-        .catch(error => console.log(error))
-  }
+      .then((response) => {
+        alert(`You have just deleted the game from your games list`);
+        navigate(`/games`);
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <div>
+         <article>
+      <div className="transactionDetails">
+        <h3>Title: {game.title}</h3>
+        <h3>Amount: ${game.price}</h3>
+        <h3>game ID: {game.esrb_rating}</h3>
+        <h5>Date: {game.release_year}</h5>
+        <h5>Location: {game.available ? "👾" : "TBD"}</h5>
+        <h5>Category: {game.category}</h5>
+      </div>
+      <div className="navigation">
+        <Link to={"/games"}>
+          <button>Back</button>
+        </Link>
+
+        <button onClick={deleteGame}>Delete</button>
+      </div>
+    </article>
 
     </div>
   );
